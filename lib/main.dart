9d7c072785af8da2eaecd9245db6f9964f58d7a5
muratdeniz2020 <73131855+muratdeniz2020@users.noreply.github.com>
2021-06-19 12:51:57 +0300

    files upload

---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.cyan[700],
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage("images/murat.jpg"),
+              ),
+              Text(
+                'Murat Deniz',
+                style: TextStyle(
+                  fontSize: 38.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'Applications developer',
+                style: TextStyle(
+                  fontSize: 19.0,
+                  color: Colors.grey.shade400,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                width: 200.0,
+                height: 20.0,
+                child: Divider(
+                  color: Colors.cyan[100],
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.all(10.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.cyan[700],
+                  ),
+                  title: Text(
+                    '+90 536 887 7895',
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.all(10.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.cyan[700],
+                  ),
+                  title: Text(
+                    'murat.deniz@ogr.altinbas.edu.tr',
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 17.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
